128e4405a7e8fd991e6ba8693eef62e759437fb1
dummy-ie <leocariomarcus@gmail.com>
2023-10-06 18:09:18 +0800

    Jump buffering now works for real, fixed movement physics feeling janky Added double jump (off by default, see allowDoubleJump bool) next to add is slope checking
    Signed-off-by: dummy-ie <leocariomarcus@gmail.com>

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -1,20 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterController2D : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private uint jumpsLeft = 0;
-    private float horizontal;
+
+    private CinemachineVirtualCamera cmVC;
+    private Cinemachine3rdPersonFollow cmTP;
+
+    private bool extraJump = false;
+    private float horizontal = 0f;
     private float coyoteTimeCounter = 0f, jumpBufferCounter = 0f;
     private bool isFacingRight = false;
     private bool isDashing = false;
     private bool isJumpPress = false;
     private float dashTime = 0f;
     private float dashSpeed = 0f;
+    private float vectorShift = 100f;
+    private bool isGrounded = false;
 
     [SerializeField] LayerMask groundLayer;
 
@@ -23,9 +27,9 @@ public class CharacterController2D : MonoBehaviour
     [Range(0, 100)][SerializeField] private float speed = 6f;
 
     [Header("Jumping")]
+    [SerializeField] private bool allowDoubleJump = false;
     [Range(0, 100)][SerializeField] private float jumpForce = 14f;
-    [SerializeField] private uint jumpLimit = 1;
-    [Range(0, 10)][SerializeField] private float fallMultiplier = 4f, lowJumpMultiplier = 1f;
+    [Range(0, 10)][SerializeField] private float fallMultiplier = 4f, lowJumpMultiplier = 0.8f;
     [Range(0, 5)][SerializeField] private float coyoteTime = 0.2f, jumpBufferTime = 0.2f;
 
     [Header("Ground Check Box Cast")]
@@ -38,8 +42,9 @@ public class CharacterController2D : MonoBehaviour
 
     private void Awake()
     {
+        cmVC = FindFirstObjectByType<CinemachineVirtualCamera>();
+        cmTP = cmVC.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
         rb = GetComponent<Rigidbody2D>();
-        jumpsLeft = jumpLimit;
     }
 
     private void Update()
@@ -52,6 +57,10 @@ public class CharacterController2D : MonoBehaviour
     private void FixedUpdate() // move player on fixed update so collisions aren't fucky wucky
     {
         if (!isDashing) rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        // interpolate the camera towards where the player is currently moving if they are moving?
+        // propose the idea later on idk
+        if (horizontal != 0) cmTP.CameraSide = Mathf.Lerp(cmTP.CameraSide, 0.5f * (horizontal + 1f), 0.05f);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -59,18 +68,18 @@ public class CharacterController2D : MonoBehaviour
         float moveX = context.ReadValue<Vector2>().x;
         if (context.started)
         {
-            Debug.Log("Move pressed");
+            // Debug.Log("Move pressed");
         }
         else if (context.performed)
         {
-            Debug.Log("Moving now" + moveX);
+            // Debug.Log("Moving now" + moveX);
 
             if (moveX >= 0.5f || moveX <= -0.5f)
                 horizontal = moveX;
         }
         else if (context.canceled)
         {
-            Debug.Log("Move released");
+            // Debug.Log("Move released");
             horizontal = 0;
         }
     }
@@ -79,53 +88,49 @@ public class CharacterController2D : MonoBehaviour
     {
         if (context.started)
         {
+            isJumpPress = true;
             // reset jump buffer counter
             jumpBufferCounter = jumpBufferTime;
-            isJumpPress = true;
         }
         else if (context.canceled)
         {
             isJumpPress = false;
-            if (rb.velocity.y > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-                coyoteTimeCounter = 0f;
-            }
         }
     }
-
 
     private void Jump()
     {
         // while grounded, set coyote time and jump limit
-        if (IsGrounded())
+        if (isGrounded = IsGrounded())
         {
             coyoteTimeCounter = coyoteTime;
-            jumpsLeft = jumpLimit;
+            extraJump = true; // refresh double jump
         }
-        else // otherwise, count down the coyote time
-            coyoteTimeCounter -= Time.deltaTime;
+        else
+            coyoteTimeCounter -= Time.deltaTime; // otherwise, count down the coyote time
 
-        if (!isJumpPress) jumpBufferCounter -= Time.deltaTime; // when not pressing jump, count down jump buffering time
+        if (!IsGrounded())
+            jumpBufferCounter -= Time.deltaTime; // when not pressing jump, count down jump buffering time
 
-        // jump while jumps available and jump is buffered and coyote time still active 
-        if (jumpsLeft > 0f && jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
+        if (!isJumpPress && rb.velocity.y > 0f)
         {
-            // Debug.Log("Jumping!");
+            // fall when releasing the jump button early (allows low jumping)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * lowJumpMultiplier);
+            coyoteTimeCounter = 0f;
+        }
+        else if (rb.velocity.y < 0f) // fast fall over time
+        {
+            rb.velocity += (fallMultiplier - 1f) * Time.deltaTime * Physics2D.gravity * Vector2.up;
+        }
+
+        // Jump if the buffer counter is active AND if coyote time is active OR you have an extra jump AND you can double jump
+        if (jumpBufferCounter > 0f && (coyoteTimeCounter > 0f || (extraJump && allowDoubleJump)))
+        {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpBufferCounter = 0f;
-            // decrement jumps
-            if (jumpsLeft > 0f)
-                jumpsLeft--;
-        }
 
-        if (rb.velocity.y < 0) // fast fall
-        {
-            rb.velocity += (fallMultiplier - 1) * Time.deltaTime * Physics2D.gravity * Vector2.up;
-        }
-        else if (rb.velocity.y > 0 && !isJumpPress) // low jump
-        {
-            rb.velocity += (lowJumpMultiplier - 1) * Time.deltaTime * Physics2D.gravity * Vector2.up;
+            if (!isGrounded && coyoteTimeCounter <= 0f)
+                extraJump = false;
         }
     }
 
--- Assets/Scripts/Player/Stuck in Between.cs
+++ Assets/Scripts/Player/Stuck in Between.cs
@@ -55,24 +55,9 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
                     ""initialStateCheck"": false
                 },
                 {
-
                     ""name"": ""Jump"",
                     ""type"": ""Button"",
                     ""id"": ""bda27d66-8cb7-4571-9076-6e5a8800bfd8"",
-
-                    ""name"": ""Dash"",
-                    ""type"": ""Button"",
-                    ""id"": ""8ebc5c95-794a-4378-ad8b-15a421327bd7"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": ""Press"",
-                    ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""Jump"",
-                    ""type"": ""Button"",
-                    ""id"": ""ad2b5195-d058-488a-97fe-9f903ca44108"",
-
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """",
@@ -267,44 +252,19 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-
                     ""id"": ""00cc23b8-4066-4868-8d6b-9f1309ebac52"",
                     ""path"": ""<Keyboard>/z"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Jump"",
-
-                    ""id"": ""98620a89-5c28-454a-afca-2b14ade63ddb"",
-                    ""path"": ""<Keyboard>/leftShift"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Keyboard&Mouse"",
-                    ""action"": ""Dash"",
-
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
                 {
                     ""name"": """",
-
                     ""id"": ""b35305ad-f5e9-41f6-9b00-9408af46fdec"",
                     ""path"": ""<Gamepad>/buttonSouth"",
-
-                    ""id"": ""331dafd1-a05b-422b-aafc-5abb169c43ef"",
-                    ""path"": """",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Dash"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""2771cdad-4518-4f74-9909-b3f9821004ea"",
-                    ""path"": ""<Keyboard>/space"",
-
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -910,10 +870,8 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
-
-        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
-
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -990,10 +948,8 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
-
-    private readonly InputAction m_Player_Dash;
-
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Dash;
     public struct PlayerActions
     {
         private @StuckinBetween m_Wrapper;
@@ -1001,10 +957,8 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
-
-        public InputAction @Dash => m_Wrapper.m_Player_Dash;
-
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -1023,14 +977,12 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
             @Fire.started += instance.OnFire;
             @Fire.performed += instance.OnFire;
             @Fire.canceled += instance.OnFire;
-
-            @Dash.started += instance.OnDash;
-            @Dash.performed += instance.OnDash;
-            @Dash.canceled += instance.OnDash;
-
             @Jump.started += instance.OnJump;
             @Jump.performed += instance.OnJump;
             @Jump.canceled += instance.OnJump;
+            @Dash.started += instance.OnDash;
+            @Dash.performed += instance.OnDash;
+            @Dash.canceled += instance.OnDash;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -1044,7 +996,6 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
             @Fire.started -= instance.OnFire;
             @Fire.performed -= instance.OnFire;
             @Fire.canceled -= instance.OnFire;
-
             @Jump.started -= instance.OnJump;
             @Jump.performed -= instance.OnJump;
             @Jump.canceled -= instance.OnJump;
@@ -1236,10 +1187,8 @@ public partial class @StuckinBetween: IInputActionCollection2, IDisposable
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
-
-        void OnDash(InputAction.CallbackContext context);
-
         void OnJump(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
