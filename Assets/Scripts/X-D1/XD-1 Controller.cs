580de4cfa20ccba7d4729bc787a4d55df1d8af27
Arkaenaz <flooper2234@gmail.com>
2023-10-06 20:17:18 +0800

    using SmoothDamp instead of LerpUnclamped for follow

--- Assets/Scripts/X-D1/XD-1 Controller.cs
+++ Assets/Scripts/X-D1/XD-1 Controller.cs
@@ -5,25 +5,35 @@ using UnityEngine.AI;
 
 public class XD1Controller : MonoBehaviour
 {
-    //[SerializeField] private float acceleration;
-    /*[SerializeField]*/ private float speed = 1;
-    [SerializeField] private float maxVelocity;
+    [SerializeField] private float acceleration;
+    //*[SerializeField]*/ private float speed = 1;
+    //[SerializeField] private float maxVelocity;
     [SerializeField] private Vector3 offsetFromPlayer;
-    [SerializeField] private float springConstant;
+    //[SerializeField] private float springConstant;
+    [SerializeField] private float catchUpTime = 0.4f;
     //private float velocity = 0;
-    //private float velocity = 0;
+    private Vector3 velocity;
     //private SpringJoint springJoint;
     private GameObject player;
     //private NavMeshAgent companionMesh;
+    private Rigidbody rb;
+    private float ticks;
+    
+    private enum State {
+        IDLE,
+        FOLLOW,
+    }
 
-    public static float EaseInOutBack(float x) {
+    private State state;
+
+    /*public static float EaseInOutBack(float x) {
         const float c1 = 1.70158f;
         const float c2 = c1 * 1.525f;
         float x2 = x - 1f;
         return x < 0.5
             ? x * x * 2 * ((c2 + 1) * x * 2 - c2)
             : x2* x2 *2 * ((c2 + 1) * x2 * 2 + c2) + 1;
-    }
+    }*/
 
     private void FollowPlayer() {
         Vector3 target;
@@ -47,12 +57,30 @@ public class XD1Controller : MonoBehaviour
         }
         //Vector2 D*/
         
-        transform.position = Vector2.LerpUnclamped(transform.position, target, EaseInOutBack(speed) * Time.deltaTime);
+        //transform.position = Vector2.LerpUnclamped(transform.position, target, EaseInOutBack(speed) * Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, catchUpTime);
+        if (velocity.magnitude <= 1f) {
+            ticks += Time.deltaTime;
+            if (ticks > 2)
+                state = State.IDLE;
+        }
+        else {
+            ticks = 0;
+        }
+    }
+
+    private void Idle() { 
+        if (Vector3.Distance(this.transform.position, player.transform.position) >= 2) {
+            state = State.FOLLOW;
+        }
     }
 
     // Start is called before the first frame update
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        state = State.IDLE;
+        ticks = 0;
         //springJoint = GetComponent<SpringJoint>();
         //companionMesh = GetComponent<NavMeshAgent>();
     }
@@ -61,7 +89,11 @@ public class XD1Controller : MonoBehaviour
     void FixedUpdate() {
         if (player == null)
             return;
-        FollowPlayer();
+        if (state == State.FOLLOW)
+            FollowPlayer();
+        if (state == State.IDLE)
+            Idle();
+
     }
 
 }
