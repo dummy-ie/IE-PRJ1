d2831487c25098755395c4b579ef04d269d00055
Arkaenaz <flooper2234@gmail.com>
2023-10-17 19:34:41 +0800

    player hud

---
+++ Assets/Scripts/Custom Control/PlayerHUD Setup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerUISetup : MonoBehaviour
+{
+    private void OnEnable() { 
+        UIDocument document = GetComponent<UIDocument>();
+        VisualElement root = document.rootVisualElement;
+
+        PlayerUI playerUI = new PlayerUI();
+    }
+}
---
+++ Assets/Scripts/Custom Control/PlayerHUD.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerHUD : VisualElement
+{
+    [UnityEngine.Scripting.Preserve]
+    public new class UxmlFactory : UxmlFactory<PlayerHUD> { }
+    public PlayerHUD() { 
+    
+    }
+}
