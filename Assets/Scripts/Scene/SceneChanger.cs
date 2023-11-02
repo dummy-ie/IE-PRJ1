0d7bb723f567fb5da62eb6040d17fe6c7dfa8729
dummy-ie <leocariomarcus@gmail.com>
2023-11-02 21:32:05 +0800

    Scene Changer and Connection

---
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField]
+    private SceneConnection _sceneConnection;
+
+    [SerializeField]
+    private string _targetSceneName;
+
+    [SerializeField]
+    private Transform _spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_sceneConnection == SceneConnection.ActiveConnection)
+        {
+            FindObjectOfType<CharacterController2D>().transform.position = _spawnPoint.position; // maybe store a player reference in the scriptableObject?
+        }
+
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneConnection.ActiveConnection = _sceneConnection;
+            // StartCoroutine(SceneLoader.Instance.FadeAndLoadScene(_targetSceneName));
+            SceneLoader.Instance.LoadSceneWithoutFade(_targetSceneName);
+        }
+    }
+}
