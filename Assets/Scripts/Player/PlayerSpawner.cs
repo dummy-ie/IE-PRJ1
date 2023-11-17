7c88950c4eaac8a2bdd607c0a80409493d71a710
tyrtr456 <n_pascual@dlsu.edu.ph>
2023-11-18 02:27:06 +0800

    Player Fix
    Fixed sign scaling
    Fixed window
    Added checkpoint system (use the bed first, if not player will not load in ondeath)
    Added public setter in currentHealth
    Added new folders to organize for player
    Added playerspawner singleton

---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSpawner : Singleton<PlayerSpawner>
+{   
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private string _checkPointName;
+
+    public string CheckPointName {
+        get { return this._checkPointName; }
+        set { this._checkPointName = value; }
+    }
+
+    private Vector3 _respawnPosition;
+
+    public Vector3 RespawnPosition{
+        get { return this._respawnPosition; }
+        set { this._respawnPosition = value; }
+    }
+
+
+    public void Respawn(){
+        Debug.Log("MissCandy");
+        _player = GameObject.FindGameObjectWithTag("Player");
+        SceneLoader.Instance.LoadScene(_checkPointName, true);
+        _player.transform.position = _respawnPosition;
+
+    }
+    
+
+
+}
