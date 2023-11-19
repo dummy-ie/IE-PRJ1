8a6406de719c854e690118c1be8b05791b3f05d3
tyrtr456 <n_pascual@dlsu.edu.ph>
2023-11-19 16:55:45 +0800

    Added level data and scene Data folders

---
+++ Assets/Scripts/Systems/SceneManagement/Scene Data/LevelData.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : ScriptableObject
+{
+   public bool doorOpened;
+   public bool vineOneBroken;
+   
+   public bool vineTwoBroken;
+
+   public bool vineThreeBroken;
+
+   public bool bridgeDropped;
+
+   public bool platformDropped;
+   
+   
+
+}
