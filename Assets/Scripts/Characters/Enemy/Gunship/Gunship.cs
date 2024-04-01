6e12a239f4b03c954394dfd1188dd50fcc995e98
tyrtr456 <n_pascual@dlsu.edu.ph>
2024-04-01 14:46:06 +0800

    1

--- Assets/Scripts/Characters/Enemy/Gunship/Gunship.cs
+++ Assets/Scripts/Characters/Enemy/Gunship/Gunship.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gunship : EnemyBaseShip
+public class Gunship : EnemyBase<Gunship>
 {
     [SerializeField] AttackData _attack;
 
@@ -12,120 +12,116 @@ public class Gunship : EnemyBaseShip
 
     [SerializeField] private GameObject _EMPtarget;
 
-    [SerializeField] private GameObject _playerTarget;
+    private GameObject _playerTarget;
 
-
-
+    private DeathState _deathState;
+    private PatrolState _patrolState;
     private ActivatedState _activeState;
     private ShootState _shootState;
     
 
     public float GetDirection(GameObject target)
     {
-        float value = target.transform.position.x - target.transform.position.x;
+        if(target != null){
+
+            float value = target.transform.position.x - target.transform.position.x;
             if (value < 0)
             return -1;
+
             else
             return 1;
+
+        }
+
+        return 0;
+        
+    }
+
+    public void SetTarget(GameObject target){
+        this._playerTarget = target;
     }
 
     override protected void Start()
     {
-        _stateMachine = new StateMachine();
-        _patrolState = new PatrolState(this);
+        
         _deathState = new DeathState(this);
+        _patrolState = new PatrolState(this);
         _shootState = new ShootState(this);
         _activeState = new ActivatedState(this);
 
-        this._stateMachine.ChangeState(this._activeState);
+        SwitchState(_activeState);
 
     }
 
-    public class ActivatedState : IState
+    public abstract class StateBase : EntityState<Gunship>
     {
-        private Gunship _gunship;
+        protected StateBase(Gunship entity) : base(entity) { }
+    }
 
-        public ActivatedState(Gunship gunship)
+    public class ActivatedState : StateBase
+    {
+
+        public ActivatedState(Gunship entity) : base(entity) { }
+
+        public override void Execute()
         {
-            _gunship = gunship;
-        }
-
-        public void Enter()
-        {
-
-            
-        }
-
-        public void Execute()
-        {
-            GameObject projectile = Instantiate(this._gunship._directionalEMP, new Vector3(this._gunship.transform.position.x + 0.2f * this._gunship.GetDirection(this._gunship._EMPtarget), this._gunship.transform.position.y + 0.1f, this._gunship.transform.position.z), Quaternion.identity);
+            GameObject projectile = Instantiate(_entity._directionalEMP, new Vector3(_entity.transform.position.x + 0.2f * _entity.GetDirection(_entity._EMPtarget), _entity.transform.position.y + 0.1f, _entity.transform.position.z), Quaternion.identity);
 
             // set source and target
             var temp = projectile.GetComponent<DirectionalProjectile>();
-            temp.SourcePlayer = this._gunship.gameObject;
+            temp.SourcePlayer = _entity.gameObject;
 
-            temp.SetTarget(this._gunship._EMPtarget.transform);
+            temp.SetTarget(_entity._EMPtarget.transform);
 
-            this._gunship._stateMachine.ChangeState(this._gunship._shootState);
+           _entity.SwitchState(_entity._shootState);
         }
 
+    }
 
+    public class ShootState : StateBase {
 
-        // private void PerformAttack(AttackData attack)
-        // {
-        //     Debug.Log("Railgunner performed attack");
-        //     RaycastHit2D[] hits = Physics2D.RaycastAll(_line.transform.position, _raycastDirection);
-        //     foreach (RaycastHit2D hit in hits)
-        //     {
-        //         if (hit.collider.gameObject.CompareTag("Player"))
-        //         {
-        //             hit.collider.GetComponent<CharacterController2D>().StartHit(_railgunner.gameObject, attack.damage);
-        //             /*IHittable handler = hit.collider.gameObject.GetComponent<IHittable>();
-        //             if (handler != null)
-        //             {
-        //                 Debug.Log("Player Hit");
-        //                 handler.OnHit(_railgunner.transform, attack.damage);
-        //             }*/
-        //         }
-        //     }
-        }
+        public ShootState(Gunship entity) : base(entity) { }
 
-    public class ShootState : IState{
-
-        private Gunship _gunship;
-
-        public ShootState(Gunship gunship)
-        {
-            _gunship = gunship;
-        }
-
-        public void Enter()
+        public override void Enter()
         {
 
             
         }
 
-        public void Execute()
+        public override void Execute()
         {
-            if(this._gunship._visionBehaviour.PlayerSeen == true && this._gunship._rangeBehaviour.InRange == true){
+            if(_entity._visionBehaviour.PlayerSeen == true && _entity._rangeBehaviour.InRange == true){
                 
-                GameObject projectile = Instantiate(this._gunship._directionalCannon, new Vector3(this._gunship.transform.position.x + 0.2f * this._gunship.GetDirection(this._gunship._playerTarget), this._gunship.transform.position.y + 0.1f, this._gunship.transform.position.z), Quaternion.identity);
+                GameObject projectile = Instantiate(_entity._directionalCannon, new Vector3(_entity.transform.position.x + 0.2f * _entity.GetDirection(_entity._playerTarget), _entity.transform.position.y + 0.1f, _entity.transform.position.z), Quaternion.identity);
 
                 // set source and target
                 var temp = projectile.GetComponent<DirectionalProjectile>();
-                temp.SourcePlayer = this._gunship.gameObject;
+                temp.SourcePlayer = _entity.gameObject;
+                _entity._playerTarget = _entity._visionBehaviour.detectedObject;
 
-                temp.SetTarget(this._gunship._playerTarget.transform);
+                temp.SetTarget(_entity._playerTarget.transform);
 
-                this._gunship._stateMachine.ChangeState(this._gunship._shootState);
+                //_entity.SwitchState(this);
 
             }
 
-            else {
-                this._gunship._stateMachine.ChangeState(this._gunship._patrolState);
-            }
+            // else {
+            //     _entity.SwitchState(_entity._patrolState); //switch to patrol state
+            // }
           
         }
+
+    }
+
+     public class PatrolState : StateBase {
+
+         public PatrolState(Gunship entity) : base(entity) { }
+
+    }
+
+    public class DeathState : StateBase {
+
+         public DeathState(Gunship entity) : base(entity) { }
 
     }
     
